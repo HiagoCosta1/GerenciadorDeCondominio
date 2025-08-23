fd55411147ecf1db2fdc1e0fe98adf8795402b67
Hiago <hiagocostazero@gmail.com>
2025-08-23 17:33:51 -0300

    criação Endpoints

--- src/main/java/com/GerenciadorCondominio/GerenciadorCondominio/controller/LocatatioController.java
+++ src/main/java/com/GerenciadorCondominio/GerenciadorCondominio/controller/LocatatioController.java
@@ -1,10 +1,15 @@
 package com.GerenciadorCondominio.GerenciadorCondominio.controller;
 
 
+import com.GerenciadorCondominio.GerenciadorCondominio.dto.LocatarioDto;
 import com.GerenciadorCondominio.GerenciadorCondominio.service.LocatarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/locatarios")
@@ -12,6 +17,47 @@ public class LocatatioController {
 
     @Autowired
     private LocatarioService locatarioService;
+
+
+    @GetMapping
+    public ResponseEntity<List<LocatarioDto>> getAll(){
+        List<LocatarioDto> listLocatatios = locatarioService.getAll();
+        return ResponseEntity.status(HttpStatus.OK).body(listLocatatios);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LocatarioDto> getById(@PathVariable ("id") long id){
+        Optional<LocatarioDto> dto = locatarioService.findById(id);
+        return dto.map(ResponseEntity::ok).orElseGet(()-> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<LocatarioDto> save(@RequestBody LocatarioDto dto){
+        LocatarioDto salvo = locatarioService.save(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(salvo);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable ("id") long id){
+        try{
+            locatarioService.delete(id);
+            return ResponseEntity.noContent().build();
+        }
+        catch (RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<LocatarioDto> update(@PathVariable("id") long id, @RequestBody  LocatarioDto dto){
+        try{
+            LocatarioDto atualizado = locatarioService.update(id, dto);
+            return ResponseEntity.ok(atualizado);
+        }
+        catch (RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 
 }
--- src/main/java/com/GerenciadorCondominio/GerenciadorCondominio/service/LocatarioService.java
+++ src/main/java/com/GerenciadorCondominio/GerenciadorCondominio/service/LocatarioService.java
@@ -1,9 +1,15 @@
 package com.GerenciadorCondominio.GerenciadorCondominio.service;
 
 
+import com.GerenciadorCondominio.GerenciadorCondominio.dto.LocatarioDto;
+import com.GerenciadorCondominio.GerenciadorCondominio.model.Locatario;
 import com.GerenciadorCondominio.GerenciadorCondominio.repository.LocatarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class LocatarioService {
@@ -13,6 +19,43 @@ public class LocatarioService {
     public LocatarioService(LocatarioRepository locatarioRepository){
         this.locatarioRepository = locatarioRepository;
     }
+
+    public List<LocatarioDto> getAll(){
+        return locatarioRepository.findAll().stream().map(locatario -> new LocatarioDto(locatario)).collect(Collectors.toUnmodifiableList());
+    }
+
+    public Optional<LocatarioDto> findById(long id){
+        return locatarioRepository.findById(id).map(LocatarioDto::new);
+
+    }
+
+    public LocatarioDto save (LocatarioDto dto){
+        Locatario locatario = new Locatario();
+        BeanUtils.copyProperties(dto, locatario);
+
+        Locatario salvo = locatarioRepository.save((locatario));
+
+        return new LocatarioDto(salvo);
+    }
+
+    public void delete(Long id){
+        Locatario locatario = locatarioRepository.findById(id).orElseThrow(()-> new RuntimeException("Locatario não encontrado"));
+
+        locatarioRepository.delete(locatario);
+    }
+
+    public LocatarioDto update(Long id, LocatarioDto dto){
+        Locatario locatarioExistente = locatarioRepository.findById(id).orElseThrow(()-> new RuntimeException("Locatario não encontrado"));
+
+        BeanUtils.copyProperties(dto,locatarioExistente, "id");
+        Locatario atualizado = locatarioRepository.save(locatarioExistente);
+
+        return new LocatarioDto(atualizado);
+    }
+
+
+
+
 
 
 
